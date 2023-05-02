1450308325f1f589d2edceebab5e3e1a6175994a
Rene Osman <rosman21@gmail.com>
2023-05-01 20:41:16 -0400

    integrated react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Electron, Typescript, Vite and React.</p>
+      </header>
+    </div>
+  );
+};
